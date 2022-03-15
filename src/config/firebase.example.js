1ed3a02730f478fddbdd950cc3012a6fbea47469
Martijn Atema <martijn@atema.one>
2022-03-15 16:42:42 +0100

    Add config examples

---
+++ src/config/firebase.example.js
@@ -0,0 +1,16 @@
+/**
+ * Copy the configuration from the project settings in the Firebase console,
+ * and rename to firebase.js
+ */
+
+const firebaseConfig = {
+  apiKey: "???",
+  authDomain: "???",
+  databaseURL: "???",
+  projectId: "???",
+  storageBucket: "???",
+  messagingSenderId: "???",
+  appId: "???"
+};
+
+export default firebaseConfig;
---
+++ src/config/spotify.example.js
@@ -0,0 +1,10 @@
+/**
+ * Copy the client id from the Spotify for developers dashboard,
+ * and rename to spotify.js
+ */
+
+const spotifyConfig = {
+  clientId: "???"
+}
+
+export default spotifyConfig;
