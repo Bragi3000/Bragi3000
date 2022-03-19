f564d7fc036792290e23dfb89089ee763a8be12e
Martijn Atema <martijn@atema.one>
2022-03-19 23:46:21 +0100

    Add init code & providers for fb/redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import { Provider } from 'react-redux';
+import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
 
 import { App, LandingPage } from 'Pages';
+import firebaseConfig from 'Config/firebase';
+import store from 'Store/store';
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/app" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <ReactReduxFirebaseProvider
+        firebase={firebase}
+        config={{ userProfile: "users" }}
+        dispatch={store.dispatch}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/app" element={<App />} />
+          </Routes>
+        </BrowserRouter>
+      </ReactReduxFirebaseProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
+
+window.firebase = firebase;
