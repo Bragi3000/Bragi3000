c0613c7813ad9664d3bbda413eca739090f259a7
Nikolai Limbrunner <ntli@kth.se>
2022-03-24 12:10:39 +0100

    Refac: move code and apply ESLint (#7)

---
+++ src/Components/TicTacToe/Square.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Square({value, onClick}) {
+  const extendedStyle = {
+    ...style,
+    color: value === "X" ? "red" : "blue",
+  }
+  return (
+    <button className="Square" style={extendedStyle} onClick={onClick}>
+      {value}
+    </button>
+  )
+}
+
+const style = {
+  fontSize: "50px"
+};
