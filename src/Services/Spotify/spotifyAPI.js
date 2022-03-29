28eb3deb7d7500528dd76e42ed1ddaa5a554bc07
vanFrZy <fred.vdw.pro@gmail.com>
2022-03-29 14:28:27 +0200

    Fix: SpotifyApi returns promises

--- src/Services/Spotify/spotifyAPI.js
+++ src/Services/Spotify/spotifyAPI.js
@@ -2,46 +2,28 @@ import spotifyConfig from "Config/spotify";
 
 import SpotifyWebApi from "spotify-web-api-node";
 
-function promiseDataHandler(data) {
-  console.log(data);
-  return data.body;
-}
-
-function promiseErrorHandler(err) {
-  console.log(err);
-  throw new Error(`API problem: ${err}`);
-}
-
 // https://developer.spotify.com/documentation/web-api/reference/#/operations/get-information-about-the-users-current-playback
 function getPlaybackState(token) {
   return new SpotifyWebApi({'accessToken':token})
     .getMyCurrentPlaybackState()
-    .then(promiseDataHandler)
-    .catch(promiseErrorHandler);
 }
 
 // https://developer.spotify.com/documentation/web-api/reference/#/operations/add-to-queue
 function addSongToQueue(token, songUri) {
   return new SpotifyWebApi({'accessToken':token})
     .addToQueue(songUri)
-    .then(promiseDataHandler)
-    .catch(promiseErrorHandler);
 }
 
 // https://developer.spotify.com/documentation/web-api/reference/#/operations/pause-a-users-playback
 function pauseSong(token) {
   return new SpotifyWebApi({'accessToken':token})
     .pause()
-    .then(promiseDataHandler)
-    .catch(promiseErrorHandler);
 }
 
 // https://developer.spotify.com/documentation/web-api/reference/#/operations/start-a-users-playback
 function playSong(token) {
   return new SpotifyWebApi({'accessToken':token})
     .play()
-    .then(promiseDataHandler)
-    .catch(promiseErrorHandler);
 }
 
 // https://developer.spotify.com/documentation/web-api/reference/#/operations/search
@@ -49,8 +31,6 @@ function playSong(token) {
 function searchSong(token, songName) {
   return new SpotifyWebApi({'accessToken':token})
     .searchTracks(songName)
-    .then(promiseDataHandler)
-    .catch(promiseErrorHandler);
 }
 
 export { getPlaybackState, addSongToQueue, playSong, pauseSong, searchSong };
