0140665906b9b09dc7798ab8aa967388549775ff
f_rzy <fred.vdw.pro@gmail.com>
2022-03-30 20:37:23 +0200

    Feat: update current playing song #30

--- src/Components/SpotifyControl/SpotifyControl.js
+++ src/Components/SpotifyControl/SpotifyControl.js
@@ -7,7 +7,12 @@ import SpotifyControlView from "./SpotifyControlView";
 const SpotifyControl = function () {
   const token = useSpotifyAuth();
   const [playbackState, setPlaybackState] = useState({
-    image_src: bragiIcon, name: "Bragi", artists: "3000", is_playing: false,
+    image_src: bragiIcon,
+    name: "Bragi",
+    artists: "3000",
+    is_playing: false,
+    progress_ms: 0,
+    duration_ms: 42069,
   });
 
   useEffect(() => {
@@ -19,12 +24,22 @@ const SpotifyControl = function () {
     }
   }, [token]);
 
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      updatePlaybackState();
+    }, Math.max(playbackState.duration_ms - playbackState.progress_ms, 15000));
+    return () => clearInterval(interval);
+  }, [playbackState]);
+
   const handleData = (newData) => {
     return {
       image_src: newData.item.album.images[0].url,
       name: newData.item.name,
       artists: newData.item.artists.map((artist) => artist.name).join(", "),
       is_playing: newData.is_playing,
+      progress_ms: newData.progress_ms,
+      duration_ms: newData.item.duration_ms,
     };
   }
 
@@ -33,8 +48,8 @@ const SpotifyControl = function () {
       .then((newData) => {
         if (newData.statusCode === 200) setPlaybackState(handleData(newData.body));
       }).catch((err) => {
-        console.log(err);
-      });
+      console.log(err);
+    });
   }
 
   const handlePlay = () => {
