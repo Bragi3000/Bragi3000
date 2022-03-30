f8f3fa51d34b7ebdfcba90a17ab68127a1c1d53d
Nikolai Limbrunner <ntli@kth.se>
2022-03-30 20:13:14 +0200

    Refac and clean-up (#29)

--- src/Components/SpotifyControl/SpotifyControl.js
+++ src/Components/SpotifyControl/SpotifyControl.js
@@ -6,51 +6,52 @@ import SpotifyControlView from "./SpotifyControlView";
 
 const SpotifyControl = function () {
   const token = useSpotifyAuth();
-
   const [playbackState, setPlaybackState] = useState({
-    image_src: bragiIcon,
-    name: "Bragi",
-    artists: "3000",
-    is_playing: false,
+    image_src: bragiIcon, name: "Bragi", artists: "3000", is_playing: false,
   });
 
   useEffect(() => {
     if (token) {
       getPlaybackState(token.access_token)
         .then((newData) => {
-          if (newData.statusCode === 200)
-            setPlaybackState(newData.body);
+          if (newData.statusCode === 200) setPlaybackState(handleData(newData.body));
         });
     }
   }, [token]);
 
   const handleData = (newData) => {
     return {
-        image_src: newData.item.album.images[0].url,
-        name: newData.item.name,
-        artist: newData.item.artists.map((artist)=>artist.name).join(", "),
-        is_playing: newData.is_playing,
-      };
+      image_src: newData.item.album.images[0].url,
+      name: newData.item.name,
+      artists: newData.item.artists.map((artist) => artist.name).join(", "),
+      is_playing: newData.is_playing,
+    };
   }
 
-  const handlePlay = () => {
-    playbackState.is_playing ? pauseSong(token.access_token) : playSong(token.access_token);
+  const updatePlaybackState = () => {
     getPlaybackState(token.access_token)
       .then((newData) => {
-        if (newData.statusCode === 200)
-          setPlaybackState(newData);
+        if (newData.statusCode === 200) setPlaybackState(handleData(newData.body));
+      }).catch((err) => {
+        console.log(err);
       });
   }
 
-  return (
-    <SpotifyControlView
-      onModifyPlayback={handlePlay}
-      imageSrc = {playbackState.image_src}
-      name = {playbackState.name}
-      artists = {playbackState.artists}
-      is_playing = {playbackState.is_playing}
-    />
-  )
+  const handlePlay = () => {
+    if (playbackState.is_playing) {
+      pauseSong(token.access_token).then(updatePlaybackState);
+    } else {
+      playSong(token.access_token).then(updatePlaybackState);
+    }
+  }
+
+  return (<SpotifyControlView
+    onModifyPlayback={handlePlay}
+    imageSrc={playbackState.image_src}
+    name={playbackState.name}
+    artists={playbackState.artists}
+    is_playing={playbackState.is_playing}
+  />)
 }
 
 export default SpotifyControl;
