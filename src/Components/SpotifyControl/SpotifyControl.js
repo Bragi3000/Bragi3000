d8200fedc6eebe29f92d9508f696d868e91d189b
Nikolai Limbrunner <ntli@kth.se>
2022-03-30 19:52:54 +0200

    Refac: implement feedback (#29)

--- src/Components/SpotifyControl/SpotifyControl.js
+++ src/Components/SpotifyControl/SpotifyControl.js
@@ -7,45 +7,51 @@ import SpotifyControlView from "./SpotifyControlView";
 const SpotifyControl = function () {
   const token = useSpotifyAuth();
 
-  const [playBackState, setPlayBackState] = useState(null);
-  const [data, setData] = useState({
-    "image_src": bragiIcon,
-    "name": "Bragi",
-    "artist": "3000",
-    "is_playing": false,
+  const [playbackState, setPlaybackState] = useState({
+    image_src: bragiIcon,
+    name: "Bragi",
+    artist: "3000",
+    isPlaying: false,
   });
-  const [isPlaying, setIsPlaying] = useState(false)
 
   useEffect(() => {
     if (token) {
-      setPlayBackState(getPlaybackState(token.access_token).then(handleData));
+      getPlaybackState(token.access_token)
+        .then((newData) => {
+          if (newData.statusCode === 200)
+            setPlaybackState(newData.body);
+        });
     }
   }, [token]);
 
-  const handleData = (playbackData) => {
-    if (playbackData.statusCode === 200) {
-      setData({
-        "image_src": playbackData.body.item.album.images[0].url,
-        "name": playbackData.body.item.name,
-        "artist": playbackData.body.item.artists.map((artist)=>artist.name).join(", "),
-        "is_playing": playbackData.body.is_playing,
-      });
-      setIsPlaying(playbackData.body.is_playing);
-      return playbackData.body;
-    }
-  }
+  // const handleData = (playbackData) => {
+  //   if (playbackData.statusCode === 200) {
+  //     setPlaybackState({
+  //       image_src: playbackData.item.album.images[0].url,
+  //       name: playbackData.item.name,
+  //       artist: playbackData.item.artists.map((artist)=>artist.name).join(", "),
+  //       is_playing: playbackData.is_playing,
+  //     });
+  //     return playbackData.body;
+  //   }
+  // }
 
   const handlePlay = () => {
-    isPlaying ? pauseSong(token.access_token) : playSong(token.access_token);
-    setPlayBackState(getPlaybackState(token.access_token));
-    setData({...data, "is_playing": !isPlaying})
-    setIsPlaying(!isPlaying);
+    playbackState.is_playing ? pauseSong(token.access_token) : playSong(token.access_token);
+    getPlaybackState(token.access_token)
+      .then((newData) => {
+        if (newData.statusCode === 200)
+          setPlaybackState(newData.body);
+      });
   }
 
   return (
     <SpotifyControlView
       onModifyPlayback={handlePlay}
-      data={data}
+      imageSrc = {playbackState.item.album.images[0].url}
+      name = {playbackState.name}
+      artists = {playbackState.item.artists.map((artist)=>artist.name).join(", ")}
+      is_playing = {playbackState.is_playing}
     />
   )
 }
--- src/Components/SpotifyControl/SpotifyControlView.js
+++ src/Components/SpotifyControl/SpotifyControlView.js
@@ -3,15 +3,15 @@ import playIcon from "Assets/images/play-icon.png";
 import pauseIcon from "Assets/images/pause-icon.png";
 
 
-const SpotifyControlView = function ({onModifyPlayback, data}) {
+const SpotifyControlView = function ({onModifyPlayback, name, imageSrc, is_playing, artists}) {
   return (
     <div className={styles.controlBar}>
-      <img src={data.image_src} alt={"_"}/>
+      <img src={imageSrc} alt={"_"}/>
       <div className={styles.containerRow}>
-        <span className={"title"}> {data.name} </span>
-        <span className={"artist"}> {data.artist} </span>
+        <span className={"title"}> {name} </span>
+        <span className={"artist"}> {artists} </span>
       </div>
-      <img src={data.is_playing ? pauseIcon : playIcon} alt={"_"} onClick={onModifyPlayback}/>
+      <img src={is_playing ? pauseIcon : playIcon} alt={"_"} onClick={onModifyPlayback}/>
     </div>
   )
 }
