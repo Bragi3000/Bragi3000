97270690ee3beaf5fdfa90f6a3d53f02b906a29a
f_rzy <fred.vdw.pro@gmail.com>
2022-03-30 19:57:51 +0200

    Fix: initial data handling for View

--- src/Components/SpotifyControl/SpotifyControl.js
+++ src/Components/SpotifyControl/SpotifyControl.js
@@ -10,8 +10,8 @@ const SpotifyControl = function () {
   const [playbackState, setPlaybackState] = useState({
     image_src: bragiIcon,
     name: "Bragi",
-    artist: "3000",
-    isPlaying: false,
+    artists: "3000",
+    is_playing: false,
   });
 
   useEffect(() => {
@@ -24,33 +24,30 @@ const SpotifyControl = function () {
     }
   }, [token]);
 
-  // const handleData = (playbackData) => {
-  //   if (playbackData.statusCode === 200) {
-  //     setPlaybackState({
-  //       image_src: playbackData.item.album.images[0].url,
-  //       name: playbackData.item.name,
-  //       artist: playbackData.item.artists.map((artist)=>artist.name).join(", "),
-  //       is_playing: playbackData.is_playing,
-  //     });
-  //     return playbackData.body;
-  //   }
-  // }
+  const handleData = (newData) => {
+    return {
+        image_src: newData.item.album.images[0].url,
+        name: newData.item.name,
+        artist: newData.item.artists.map((artist)=>artist.name).join(", "),
+        is_playing: newData.is_playing,
+      };
+  }
 
   const handlePlay = () => {
     playbackState.is_playing ? pauseSong(token.access_token) : playSong(token.access_token);
     getPlaybackState(token.access_token)
       .then((newData) => {
         if (newData.statusCode === 200)
-          setPlaybackState(newData.body);
+          setPlaybackState(newData);
       });
   }
 
   return (
     <SpotifyControlView
       onModifyPlayback={handlePlay}
-      imageSrc = {playbackState.item.album.images[0].url}
+      imageSrc = {playbackState.image_src}
       name = {playbackState.name}
-      artists = {playbackState.item.artists.map((artist)=>artist.name).join(", ")}
+      artists = {playbackState.artists}
       is_playing = {playbackState.is_playing}
     />
   )
