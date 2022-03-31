be0a514f9665be4be03e773bee5a6fc980e51433
Martijn Atema <martijn@atema.one>
2022-03-31 15:43:24 +0200

    View for song selector search results

--- src/Components/SongSelector/SearchResultsView.js
+++ src/Components/SongSelector/SearchResultsView.js
@@ -1,5 +1,30 @@
-const SearchResultsView = function () {
-  return <></>;
+const SearchResultsView = function ({
+  songs = [],
+  onSelectSong: onSongSelect = (song) => {},
+}) {
+  return (
+    <div>
+      {songs.map((song) => {
+        const handleClick = (e) => {
+          e.preventDefault();
+          onSongSelect(song);
+        };
+
+        return (
+          <a key={song.id} href="_" onClick={handleClick}>
+            <div>
+              <img src={song.album.images[2].url} alt="" />
+              <span>{song.name}</span>
+              <br />
+              <span>
+                {song.artists.map((artist) => artist.name).join(", ")}
+              </span>
+            </div>
+          </a>
+        );
+      })}
+    </div>
+  );
 };
 
 export default SearchResultsView;
