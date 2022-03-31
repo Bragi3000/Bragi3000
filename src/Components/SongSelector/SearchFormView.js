287a712140fb979136a4dd38a958de4ea88b39a1
Martijn Atema <martijn@atema.one>
2022-03-31 14:57:06 +0200

    View for song selector search form

--- src/Components/SongSelector/SearchFormView.js
+++ src/Components/SongSelector/SearchFormView.js
@@ -1,5 +1,26 @@
-const SearchFormView = function () {
-  return <></>;
+import { useRef } from "react";
+
+const SearchFormView = function ({
+  onQueryChange = (query) => {},
+  onSearch = (query) => {},
+}) {
+  const query = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSearch(query.current.value);
+  };
+
+  const handleChange = (e) => {
+    onQueryChange(e.target.value);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="Search song" ref={query} onChange={handleChange} />
+      <input type="submit" value="Search!" />
+    </form>
+  );
 };
 
 export default SearchFormView;
