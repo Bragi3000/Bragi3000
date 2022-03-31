73bf7039dbf852842cb3dd9daf8c93e9ceccf5e8
Martijn Atema <martijn@atema.one>
2022-03-31 15:57:07 +0200

    View for song selector selected song

--- src/Components/SongSelector/SelectedSongView.js
+++ src/Components/SongSelector/SelectedSongView.js
@@ -1,5 +1,20 @@
-const SelectedSongView = function () {
-  return <></>;
+const SelectedSongView = function ({
+  song,
+  isConfirmed = false,
+  onConfirm = () => {},
+  onCancel = () => {},
+}) {
+  return (
+    <div>
+      <img src={song.album.images[2].url} alt="" />
+      <span>{song.name}</span>
+      <br />
+      <span>{song.artists.map((artist) => artist.name).join(", ")}</span>
+      <br />
+      <button disabled={isConfirmed} onClick={() => onConfirm()}>Confirm</button>
+      <button hidden={isConfirmed} onClick={() => onCancel()}>Cancel</button>
+    </div>
+  );
 };
 
 export default SelectedSongView;
