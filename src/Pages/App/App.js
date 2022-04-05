75c183b7d482fc9d08f328e45366ccebeaa7f006
Nikolai Limbrunner <ntli@kth.se>
2022-04-05 13:08:35 +0200

    Feat: add tictactoe component (#44)

--- src/Pages/App/App.js
+++ src/Pages/App/App.js
@@ -4,6 +4,7 @@ import SpotifyControl from "Components/SpotifyControl/SpotifyControl";
 import useUserData from "Services/firebase/useUserData";
 import SongSelector from "Components/SongSelector/SongSelector";
 import { LEFT_PLAYER, RIGHT_PLAYER } from "Constants/players";
+import TicTacToe from "../../Components/TicTacToe/TicTacToe";
 
 const App = function () {
   const user = useUserData();
@@ -19,6 +20,8 @@ const App = function () {
       <SpotifyControl />
 
       <SongSelector player={LEFT_PLAYER} />
+
+      <TicTacToe size={3}/>
 
       <SongSelector player={RIGHT_PLAYER} />
     </div>
