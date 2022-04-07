15b89e9e3060e32d349dbedb1b76f71d553ac7fb
Nikolai Limbrunner <ntli@kth.se>
2022-04-07 10:13:55 +0200

    Docs: more docs to Components

--- src/Components/WaitingView/WaitingView.js
+++ src/Components/WaitingView/WaitingView.js
@@ -1,6 +1,11 @@
 import styles from "./WaitingView.module.css";
 import spinner from "Assets/images/spinner.svg";
 
+/**
+ * Component which is shown by the other components when the required data is not available yet
+ * Displays a spinner and a message
+ * @param text - text which is shown in the waiting view
+ */
 const WaitingView = function ({ text = "Waiting..." }) {
   return (
     <div className={styles.view}>
