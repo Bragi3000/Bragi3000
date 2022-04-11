c2831c80629ed178b91b52c3c0b23c0a9c05a555
Nikolai Limbrunner <ntli@kth.se>
2022-04-11 21:18:17 +0200

    Feat: extend to spotifyApi with playlist functions (#8)

--- src/Services/Spotify/spotifyAPI.js
+++ src/Services/Spotify/spotifyAPI.js
@@ -63,4 +63,75 @@ async function searchSong(accessToken, searchQuery) {
   return response.body.tracks?.items ?? [];
 }
 
-export { getPlaybackState, addSongToQueue, playSong, pauseSong, searchSong };
+const playlistName = "bragi3000"
+
+/**
+ * Method which calls the spotify API to create a playlist
+ * https://developer.spotify.com/documentation/web-api/reference/#create-playlist
+ * @param {String} accessToken valid spotify Bearer type access token
+ * @param {String} playlistName name of the playlist to be created - standard is bragi3000
+ * @returns {Promise} Promise for the spotify user object returned by the API (user part of the API response)
+ * @throws Will reject if the response status code is not 201
+ */
+function getSpotifyUser(accessToken) {
+  return new SpotifyWebApi({ accessToken }).getMe();
+}
+
+
+/**
+ * Method which calls the spotify API to get a list of playlists of the authenticated user
+ * https://developer.spotify.com/documentation/web-api/reference/#get-a-list-of-current-users-playlistso
+ * @param {String} accessToken valid spotify Bearer type access token
+ * @param {String} userId id of the user whose playlists are to be retrieved
+ * @returns {Promise} Promise for the list of playlists returned by the API
+ */
+function getUserPlaylists(accessToken, userId) {
+  return new SpotifyWebApi({accessToken}).getUserPlaylists(userId);
+}
+
+
+/**
+ * Method which calls the spotify API to create a playlist.
+ * https://developer.spotify.com/documentation/web-api/reference/#create-playlist
+ * @param {String} accessToken valid spotify Bearer type access token
+ * @returns {Promise} Promise for the newly created playlist object returned by the API
+ */
+function createPlaylist(accessToken) {
+  return new SpotifyWebApi({ accessToken }).createPlaylist(playlistName, { 'public': false });
+}
+
+/**
+ * Method which calls the spotify API to get a playlist.
+ * https://developer.spotify.com/documentation/web-api/reference/#get-playlist-tracks
+ * @param {String} accessToken valid spotify Bearer type access token
+ * @param {String} playlistId id of the playlist to be retrieved
+ * @returns {Promise} Promise for the playlist object returned by the API
+ */
+function getPlaylist(accessToken, playlistId) {
+  return new SpotifyWebApi({ accessToken }).getPlaylist(playlistId);
+}
+
+/**
+ * Method which calls the spotify API to add a track to a playlist.
+ * @param accessToken valid spotify Bearer type access token
+ * @param playlistId id of the playlist to song will be added to
+ * @param trackUri uri of the track to be added
+ * @returns {Promise} Promise for the songs added to the playlist
+ */
+function addSongToPlaylist(accessToken, playlistId, trackUri) {
+  return new SpotifyWebApi({ accessToken }).addTracksToPlaylist(playlistId, [trackUri]);
+}
+
+/**
+ * Method which calls the spotify API to delete multiple tracks from a playlist.
+ * @param accessToken valid spotify Bearer type access token
+ * @param playlistId id of the playlist to songs will be deleted from
+ * @param trackUris uris of the tracks to be deleted
+ * @param snapshotId id of the playlist snapshot
+ * @returns {Promise} Promise for the songs deleted from the playlist
+ */
+function removeSongsFromPlaylist(accessToken, playlistId, trackUris, snapshotId) {
+  return new SpotifyWebApi({ accessToken }).removeTracksFromPlaylist(playlistId, trackUris, { snapshot_id: snapshotId });
+}
+
+export { getPlaybackState, addSongToQueue, playSong, pauseSong, searchSong, createPlaylist, getPlaylist, addSongToPlaylist, getSpotifyUser, getUserPlaylists, removeSongsFromPlaylist };
