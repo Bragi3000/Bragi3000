75a5b544ebc231e5aea808699798afed1ff1f4d0
vanFrZy <fred.vdw.pro@gmail.com>
2022-05-06 16:20:24 +0200

    Docs: added docs for reducers

--- src/Store/slices/helper.js
+++ src/Store/slices/helper.js
@@ -8,20 +8,41 @@ const initialState = {
   position: 0
 };
 
+/**
+ * Slice for helper state
+ */
 const helperSlice = createSlice({
   name: "helper",
   initialState,
   reducers: {
 
+    /**
+     * Action & reducer to toggle help activity
+     */
     toggleHelpActivity: (state, action) => {
       state.helpActive = !state.helpActive;
     },
 
+    /**
+     * Action & reducer to reset the playback state
+     */
+    resetPlaybackState: () => initialState,
+
+
+    /**
+     * Action & reducer to show help for certain component position
+     */
     setHelpedComponent: (state, {payload: {explainedComponentPosition}}) => {
       state.position = explainedComponentPosition;
     }
+
   }
 })
 
-export const {toggleHelpActivity, setHelpedComponent} = helperSlice.actions;
+export const {
+  toggleHelpActivity,
+  setHelpedComponent,
+  resetPlaybackState
+} = helperSlice.actions;
+
 export default helperSlice.reducer;
