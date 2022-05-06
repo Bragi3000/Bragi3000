cc88edab734f4abfed597b2d87520ba62ff26ce0
vanFrZy <fred.vdw.pro@gmail.com>
2022-05-06 18:20:54 +0200

    Fix: remove infinite event listners on re-renders

--- src/Components/HelpButton/HelpButton.js
+++ src/Components/HelpButton/HelpButton.js
@@ -1,10 +1,16 @@
 import HelpButtonView from "./HelpButtonView"
 import {useDispatch, useSelector} from "react-redux";
 import {selectPosition, changePosition, toggleHelpActivity, selectHelpActive} from "Store/slices/helper";
+import {useEffect} from "react";
 
 const HelpButton = function () {
   const helpActive = useSelector(state => selectHelpActive(state));
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    document.body.addEventListener("keydown", handleHelperPosition);
+    return () => document.body.removeEventListener("keydown", handleHelperPosition);
+  }, [])
 
   function toggle() {
     dispatch(toggleHelpActivity());
@@ -18,7 +24,6 @@ const HelpButton = function () {
 
   }
 
-  document.body.addEventListener("keydown", handleHelperPosition);
 
   return (
     <HelpButtonView
