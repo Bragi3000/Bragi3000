d8832a4980f704c74e4ac47a7053b288de54da55
Martijn Atema <martijn@atema.one>
2022-05-09 21:13:41 +0200

    Fix landing page

--- src/Pages/LandingPage/LandingPage.js
+++ src/Pages/LandingPage/LandingPage.js
@@ -1,5 +1,5 @@
-import {Link} from "react-router-dom";
-import kermit from "Assets/images/kermit.png"
+import { Link } from "react-router-dom";
+import kermit from "Assets/images/kermit.png";
 
 /*
  * Landing page of the application. Shows a welcome message and a link to open the application.
@@ -9,13 +9,12 @@ const LandingPage = function () {
     <>
       <div className="container mx-auto px-5 my-40 text-center">
         <h1 className="text-7xl mb-8">Bragi 3000</h1>
-        <p className="text-3xl mb-6 text-gray-200 w-192">
+        <p className="text-3xl mb-6 text-gray-200 mx-auto w-192">
           Give everyone the chance to hear their favorite song
         </p>
         <p className="text-xl mb-6 text-gray-400 mx-auto w-128">
-          Two people select a song from Spotify.
-          Afterwards they play a fun minigame.
-          The song chosen by the winner is added to the queue, while
+          Two people select a song from Spotify. Afterwards they play a fun
+          minigame. The song chosen by the winner is added to the queue, while
           the other song gets obliterated.
         </p>
         <Link
@@ -25,7 +24,11 @@ const LandingPage = function () {
           Start playing
         </Link>
       </div>
-      <img src={kermit} className="fixed bottom-0 w-80 right-0 animate-kermit" alt={"kermit"}/>
+      <img
+        src={kermit}
+        className="fixed bottom-0 w-80 right-0 animate-kermit"
+        alt="Kermit"
+      />
     </>
   );
 };
